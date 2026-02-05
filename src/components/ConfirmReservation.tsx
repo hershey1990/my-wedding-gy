ca27dc51ec611b3eae931881c855e9cf47ccc5af
Gershell Lopez <grlopez90@gmail.com>
2026-02-04 20:22:49 -0600

    feat: add confirmation alert and name input to ConfirmReservation component

--- src/components/ConfirmReservation.tsx
+++ src/components/ConfirmReservation.tsx
@@ -1,4 +1,12 @@
+import { useState } from "preact/hooks";
+
 export default () => {
+  const handleSendConfirmation = () => {
+    alert("¡Gracias por confirmar tu asistencia!");
+  };
+
+  const [name, setName] = useState("");
+
   return (
     <section
       id="rsvp"
@@ -10,10 +18,22 @@ export default () => {
       </h2>
       {/* Confirmar asistencia */}
       <div class="text-2xl md:text-3xl font-thin flex space-x-4">
+        {/* boton dentro de un input para colocar nombre */}
+        <input
+          name={"name"}
+          type="text"
+          placeholder="Tu nombre"
+          class="px-4 py-2 rounded-lg border border-[var(--border-color-dark)]"
+          value={name}
+          onInput={(e) => setName((e.target as HTMLInputElement).value)}
+        />
+        {/* Este boton envia un email confirmando a la persona */}
         <a
           href="https://forms.gle/8o5Ykz1Z6Yt6n3cX9"
           target="_blank"
           class="hover:text-[var(--shade-900)] !bg-[var(--tone-300)] px-6 py-3 rounded-lg border border-[var(--border-color-dark)] transition"
+          rel="noreferrer"
+          onClick={handleSendConfirmation}
         >
           Confirmar asistencia
         </a>
