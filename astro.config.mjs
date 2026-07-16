ab3f8e907a67347ce666ab4f822b82b598dc6dab
Gershell Lopez <gershell.lopez@gettalento.com>
2026-07-16 07:34:06 -0600

    Save uncommitted changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://g-y.us',
   vite: { plugins: [tailwindcss()] },
 });
